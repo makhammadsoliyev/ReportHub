f16e9200d6cb77f0e66e402e5e7f0e59a7b7c898
umakhammadsoliev <umakhammadsoliev@TASL-00185>
2025-06-24 15:19:41 +0500

    Add project files.

---
+++ src/ReportHub.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReportHub.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    
+}
