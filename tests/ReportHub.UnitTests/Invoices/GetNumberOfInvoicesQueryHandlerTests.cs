6495bfe4e8f577a4e5fa4fd6fdd9c9dc75bef826
umidjon <makhammadsoliyev@gmail.com>
2025-07-06 01:20:42 +0500

    Test

--- tests/ReportHub.UnitTests/Invoices/GetNumberOfInvoicesQueryHandlerTests.cs
+++ tests/ReportHub.UnitTests/Invoices/GetNumberOfInvoicesQueryHandlerTests.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using MockQueryable;
 using Moq;
+using ReportHub.Application.Common.Exceptions;
 using ReportHub.Application.Common.Interfaces.Repositories;
 using ReportHub.Application.Invoices.GetNumberOfInvoices;
 using ReportHub.Domain;
@@ -71,6 +72,40 @@ public class GetNumberOfInvoicesQueryHandlerTests
 		result.Count.Should().Be(2);
 		result.StartDate.Should().Be(filter.StartDate.Value);
 		result.EndDate.Should().Be(filter.EndDate.Value);
+
+		customerRepositoryMock.Verify(
+			repository => repository.AnyAsync(It.IsAny<Expression<Func<Customer, bool>>>()), Times.Once);
+
+		invoiceRepositoryMock.Verify(
+			repository => repository.SelectAll(), Times.Once);
+	}
+
+	[Test]
+	public async Task Handle_WhenCustomerNotFound_ShouldThrowNotFoundException()
+	{
+		// Arrange
+		var filter = new GetNumberOfInvoicesFilter
+		{
+			CustomerId = Guid.NewGuid(),
+		};
+		var invoices = new List<Invoice>().BuildMock();
+
+		customerRepositoryMock.Setup(
+				repository => repository.AnyAsync(It.IsAny<Expression<Func<Customer, bool>>>()))
+			.ReturnsAsync(false);
+
+		invoiceRepositoryMock.Setup(
+				repository => repository.SelectAll())
+			.Returns(invoices);
+
+		var query = new GetNumberOfInvoicesQuery(filter, Guid.Empty);
+		var handler = new GetNumberOfInvoicesQueryHandler(invoiceRepositoryMock.Object, customerRepositoryMock.Object);
+
+		// Act
+		var result = () => handler.Handle(query, default);
+
+		// Assert
+		await result.Should().ThrowAsync<NotFoundException>();
 
 		customerRepositoryMock.Verify(
 			repository => repository.AnyAsync(It.IsAny<Expression<Func<Customer, bool>>>()), Times.Once);
